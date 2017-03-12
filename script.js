f94e6b8980f9f8e4ed63a9f6a3f30f7af56bc1b0
Ramon Bartl <rb@ridingbytes.com>
2017-03-12 20:18:50 +0100

    Initial Commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// Mouseovereffekt
+function highlightOn(node) {
+  node.style.color = '#FF23CC';
+  node.style.cursor = 'pointer';
+}
+
+function highlightOff(node) {
+  node.style.color = '#663399';
+}
+
+function fehler() {
+    self=null;
+}
